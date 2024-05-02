4aaf7e6d30d1d9b82d5f98b397509923009cc5d7
Minyoung Park <pmy2798@naver.com>
2024-05-03 06:18:08 +0900

    Ex CH03-05

--- CH_03/example_03_05.js
+++ CH_03/example_03_05.js
@@ -1,13 +1,18 @@
 var a = 1;
-global.b = 2;
+delete global.a;
 console.log(a, global.a, this.a);
+
+var b = 2;
+delete b;
 console.log(b, global.b, this.b);
 
-global.a = 3;
-b = 4;
+global.c = 3;
+delete global.c;
+console.log(c, global.c, this.c);
 
-console.log(a, global.a, this.a);
-console.log(b, global.b, this.b);
+global.d = 4;
+delete d;
+console.log(d, global.d, this.d);
 
 // 해보니까 1 undefined undefined 와.
 //         2 2 undefined 
