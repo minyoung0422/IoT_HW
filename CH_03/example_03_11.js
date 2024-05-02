e145d7124302aa409cbb7a7488391f925a33fd31
Minyoung Park <pmy2798@naver.com>
2024-05-03 07:03:57 +0900

    Ex CH03-11

---
+++ CH_03/example_03_11.js
@@ -0,0 +1,13 @@
+var obj = {
+    outer:function () {
+        console.log(this);
+        var innerFunc = () => {
+            console.log(this);
+        }
+
+        innerFunc();
+    }
+    
+};
+
+obj.outer();
