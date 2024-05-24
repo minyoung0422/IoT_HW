3f71213b3c9e0231db7ce520f899321a8fb5454c
Minyoung Park <pmy2798@naver.com>
2024-05-24 13:08:46 +0900

    Ex CH05-05

---
+++ CH_05/example_05_05.js
@@ -0,0 +1,39 @@
+var outer = (function() {
+  var a = 1;
+  var inner = function() {
+    return ++a;
+  };
+  return inner;
+})();
+console.log(outer());
+console.log(outer());
+outer = null;
+
+(function() {
+  var a = 0;
+  var intervalId = null;
+  var inner = function() {
+    if (++a >= 10) {
+      clearInterval(intervalId);
+      inner = null;
+    }
+    console.log(a);
+  };
+  intervalId = setInterval(inner, 1000);
+})();
+
+(function() {
+  var count = 0;
+  var button = document.createElement('button');
+  button.innerText = 'click';
+
+  var clickHandler = function() {
+    console.log(++count, 'times clicked');
+    if (count >= 10) {
+      button.removeEventListener('click', clickHandler);
+      clickHandler = null;
+    }
+  };
+  button.addEventListener('click', clickHandler);
+  document.body.appendChild(button);
+})();
